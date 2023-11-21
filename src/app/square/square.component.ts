89b6fd5db19cd8ed5c04a496692c1d3b04dffd32
spacemanstan <ariston.bodnarchuk@gmail.com>
2023-11-21 16:15:32 -0600

    generated sqaure component
    I deleted the boilerplate out of app.component.html and left just the router tag
    
    then I generated a new component for square using
    
    ng generate component square --inline-template true -- inline-style true

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-square',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <p>
+      square works!
+    </p>
+  `,
+  styles: ``
+})
+export class SquareComponent {
+
+}
